8eb7a1158c13dc5557c186062abd51cdbc970198
duzz17 <ducn69279@gmail.com>
2026-09-21 08:23:17 +0700

    the first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using QuanLyCuaHangDienTu_UNETI06_TI17A2HN.Models;
+
+namespace QuanLyCuaHangDienTu_UNETI06_TI17A2HN.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+        });
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuanLyCuaHangDienTu_UNETI06_TI17A2HN.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId =>
+        !string.IsNullOrEmpty(RequestId);
+}
